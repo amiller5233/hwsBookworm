6388f23773f7bbdfbfc8508548cb5c259f169759
amiller5233@gmail.com <amiller5233@gmail.com>
2023-08-07 18:35:14 -0700

    create xcode project

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Adam Miller on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
