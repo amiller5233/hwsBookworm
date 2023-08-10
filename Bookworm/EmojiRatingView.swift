ef6f164a7c1a7317bac0430f4e0c1eafa43c8811
amiller5233@gmail.com <amiller5233@gmail.com>
2023-08-09 21:19:19 -0700

    EmojiRatingView

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Adam Miller on 8/9/23.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView()
+    }
+}
